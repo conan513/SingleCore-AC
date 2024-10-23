c61211cfba23bf06dacdcf5348b54e4a412629d9
ConanHUN <conanhun@hotmail.com>
2024-10-23 11:08:40 +0200

    Playerbots update
    https://github.com/liyunfan1223/mod-playerbots/commit/599c26bc2ec20729de4f0b52f20596865ffd1ec3

--- modules/mod-playerbots/src/RandomPlayerbotMgr.cpp
+++ modules/mod-playerbots/src/RandomPlayerbotMgr.cpp
@@ -1262,9 +1262,7 @@ void RandomPlayerbotMgr::RandomTeleport(Player* bot, std::vector<WorldLocation>&
     if (botAI)
     {              
         // ignore when in when taxi with boat/zeppelin and has players nearby
-        if (bot->HasUnitMovementFlag(MOVEMENTFLAG_ONTRANSPORT) && 
-                bot->HasUnitState(UNIT_STATE_IGNORE_PATHFINDING) && 
-                    botAI->HasPlayerNearby())
+        if (botAI->IsTaxiFlying() && botAI->HasPlayerNearby())
             return;
     }
 
--- modules/mod-playerbots/src/strategy/dungeons/DungeonStrategyContext.h
+++ modules/mod-playerbots/src/strategy/dungeons/DungeonStrategyContext.h
@@ -8,22 +8,17 @@
 #include "wotlk/oldkingdom/OldKingdomStrategy.h"
 #include "wotlk/draktharonkeep/DrakTharonKeepStrategy.h"
 #include "wotlk/violethold/VioletHoldStrategy.h"
+#include "wotlk/gundrak/GundrakStrategy.h"
+#include "wotlk/hallsofstone/HallsOfStoneStrategy.h"
+#include "wotlk/hallsoflightning/HallsOfLightningStrategy.h"
+#include "wotlk/utgardepinnacle/UtgardePinnacleStrategy.h"
+#include "wotlk/cullingofstratholme/CullingOfStratholmeStrategy.h"
 
 /*
 Full list/TODO:
 
-Gundrak - GD
-Slad'ran, Drakkari Colossus, Moorabi, Gal'darah, Eck the Ferocious (Heroic only)
-Halls of Stone - HoS
-Maiden of Grief, Krystallus, Tribunal of Ages, Sjonnir The Ironshaper
-Halls of Lightning - HoL
-General Bjarngrim, Volkhan, Ionar, Loken
 The Oculus - Occ
 Drakos the Interrogator, Varos Cloudstrider, Mage-Lord Urom, Ley-Guardian Eregos
-Utgarde Pinnacle - UP
-Svala Sorrowgrave, Gortok Palehoof, Skadi the Ruthless, King Ymiron
-The Culling of Stratholme - CoS
-Meathook, Salramm the Fleshcrafter, Chrono-Lord Epoch, Mal'Ganis, Infinite Corruptor (Heroic only)
 Trial of the Champion - ToC
 Alliance Champions: Deathstalker Visceri, Eressea Dawnsinger, Mokra the Skullcrusher, Runok Wildmane, Zul'tore
 Horde Champions: Ambrose Boltspark, Colosos, Jacob Alerius, Jaelyne Evensong, Lana Stouthammer
@@ -76,14 +71,15 @@ class DungeonStrategyContext : public NamedObjectContext<Strategy>
         static Strategy* wotlk_ok(PlayerbotAI* botAI) { return new WotlkDungeonOKStrategy(botAI); }
         static Strategy* wotlk_dtk(PlayerbotAI* botAI) { return new WotlkDungeonDTKStrategy(botAI); }
         static Strategy* wotlk_vh(PlayerbotAI* botAI) { return new WotlkDungeonVHStrategy(botAI); }
-
-        static Strategy* wotlk_gd(PlayerbotAI* botAI) { return new WotlkDungeonUKStrategy(botAI); }
-        static Strategy* wotlk_hos(PlayerbotAI* botAI) { return new WotlkDungeonUKStrategy(botAI); }
-        static Strategy* wotlk_hol(PlayerbotAI* botAI) { return new WotlkDungeonUKStrategy(botAI); }
+        static Strategy* wotlk_gd(PlayerbotAI* botAI) { return new WotlkDungeonGDStrategy(botAI); }
+        static Strategy* wotlk_hos(PlayerbotAI* botAI) { return new WotlkDungeonHoSStrategy(botAI); }
+        static Strategy* wotlk_hol(PlayerbotAI* botAI) { return new WotlkDungeonHoLStrategy(botAI); }
+        // static Strategy* wotlk_occ(PlayerbotAI* botAI) { return new WotlkDungeonOccStrategy(botAI); }
         static Strategy* wotlk_occ(PlayerbotAI* botAI) { return new WotlkDungeonUKStrategy(botAI); }
-        static Strategy* wotlk_up(PlayerbotAI* botAI) { return new WotlkDungeonUKStrategy(botAI); }
-        static Strategy* wotlk_cos(PlayerbotAI* botAI) { return new WotlkDungeonUKStrategy(botAI); }
-        static Strategy* wotlk_toc(PlayerbotAI* botAI) { return new WotlkDungeonUKStrategy(botAI); }
+        static Strategy* wotlk_up(PlayerbotAI* botAI) { return new WotlkDungeonUPStrategy(botAI); }
+        static Strategy* wotlk_cos(PlayerbotAI* botAI) { return new WotlkDungeonCoSStrategy(botAI); }
+        
+        static Strategy* wotlk_toc(PlayerbotAI* botAI) { return new WotlkDungeonUKStrategy(botAI); } // NYI from here down
         static Strategy* wotlk_hor(PlayerbotAI* botAI) { return new WotlkDungeonUKStrategy(botAI); }
         static Strategy* wotlk_pos(PlayerbotAI* botAI) { return new WotlkDungeonUKStrategy(botAI); }
         static Strategy* wotlk_fos(PlayerbotAI* botAI) { return new WotlkDungeonUKStrategy(botAI); }
